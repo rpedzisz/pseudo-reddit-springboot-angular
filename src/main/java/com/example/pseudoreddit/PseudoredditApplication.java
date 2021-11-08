095dfcc044ace77d2b3cf55a8925a75b65635c9a
rpedzisz <rpedzisz@gmail.com>
2021-11-08 16:26:20 +0100

    added header from reddit html/css

--- src/main/java/com/example/pseudoreddit/PseudoredditApplication.java
+++ src/main/java/com/example/pseudoreddit/PseudoredditApplication.java
@@ -1,11 +1,14 @@
 package com.example.pseudoreddit;
 
+import com.example.pseudoreddit.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class PseudoredditApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/pseudoreddit/config/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.pseudoreddit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket pseudoRedditApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+
+    }
+    private ApiInfo getApiInfo(){
+
+    return new ApiInfoBuilder(){}
+    .title("Pseudo Reddit API")
+            .version("1")
+            .description("API for pseudo reddit application")
+            .contact(new Contact("rafal", "no url", "rpedzisz@gmail.com"))
+            .license("no license")
+            .build();
+    }
+
+
+
+
+}
